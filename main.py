41c1c08b870b327126e6bf3ac89ce2d025c3ea02
KevinAsian <55142541+KevinAsian@users.noreply.github.com>
2019-10-29 08:41:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
-import arcade
 
+import arcade
+ 
 background = arcade.Sprite('background.jpg', center_x = 320, center_y = 240, scale = 1.1)
 WIDTH = 640
 HEIGHT = 480
-
-window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+ 
 x = 50
-x_speed = 1
-
-
+x_speed = 5
+spider = arcade.Sprite('spider.png', center_x = x, center_y = 240, scale = 0.20)
+window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+ 
+ 
+ 
 def setup():
-    arcade.schedule(update, 1/60)
-    arcade.run()
-
-
-def update(delta_time):
-    pass
-
+   arcade.schedule(update, 1/60)
+   arcade.run()
+ 
+ 
+def update (delta_time):
+   global x, x_speed
+   spider.update()
+  
+ 
+  
+ 
 @window.event
 def on_draw():
-    arcade.start_render()
-    # Draw in here...
-    arcade.draw_circle_filled(x, 100, 25, arcade.color.BLUE)
-    background.draw()
-
-
+   arcade.start_render()
+   # Draw in here...
+   background.draw()
+   spider.draw()
+   if spider.center_x <= WIDTH - 50:
+       spider.center_x += x_speed
+  
+ 
 @window.event
 def on_key_press(key, modifiers):
-    pass
-
-
+   pass
+ 
+ 
 @window.event
 def on_key_release(key, modifiers):
-    pass
-
-
+   pass
+ 
+ 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-    pass
-
-
+   pass
+ 
+ 
 if __name__ == '__main__':
-    setup()
+   setup()
