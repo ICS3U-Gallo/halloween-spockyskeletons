896671a001e2dafad1fcab1b7d860093a5b526eb
Danny Lu <danny.lu075@gmail.com>
2019-10-30 09:14:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 import arcade
- 
-background = arcade.Sprite('background.jpg', center_x = 320, center_y = 240, scale = 1.1)
+
+background = arcade.Sprite('background.jpg', center_x=320, center_y=240, scale=1.1)
 WIDTH = 640
 HEIGHT = 480
- 
+
 x = 50
 x_speed = 5
-spider = arcade.Sprite('spider.png', center_x = x, center_y = 240, scale = 0.20)
+spider = arcade.Sprite('spider.png', center_x=x, center_y=240, scale=0.15)
 window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
- 
- 
- 
+
+
 def setup():
-   arcade.schedule(update, 1/60)
-   arcade.run()
- 
- 
-def update (delta_time):
-   global x, x_speed, spider
-   spider.update()
-  
-    if spider.center_x <= 0:
-       x_speed = 5
-   elif spider.center_x >= WIDTH:
+    arcade.schedule(update, 1 / 60)
+    arcade.run()
+
+
+def update(delta_time):
+    global x, x_speed, spider
+    spider.update()
+
+    if spider.center_x <= 15:
+        x_speed = 5
+
+    elif spider.center_x >= (WIDTH-15):
         x_speed = -5
-        spider.center_x += x_speed
-  
- 
+    spider.center_x += x_speed
+
+
+
 @window.event
 def on_draw():
-   arcade.start_render()
-   # Draw in here...
-   background.draw()
-   spider.draw()
-   
+    arcade.start_render()
+    # Draw in here...
+    background.draw()
+    spider.draw()
 
-  
- 
+
 @window.event
 def on_key_press(key, modifiers):
-   pass
- 
- 
+    pass
+
+
 @window.event
 def on_key_release(key, modifiers):
-   pass
- 
- 
+    pass
+
+
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-   pass
- 
- 
+    pass
+
+
 if __name__ == '__main__':
-   setup()
+    setup()
